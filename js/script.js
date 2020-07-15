f43465bb6b8e330f459882fd5691b6ae33d2dc6b
Doctrina-lucem <65225652+Doctrina-lucem@users.noreply.github.com>
2020-07-15 22:34:58 +0300

    h

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ var writeusPopup = document.querySelector(".write-us-popup");
 var popupclose = document.querySelector(".close-window");
 var catalogPopup = document.querySelector(".catalog-popup-hide");
 var catalogPopuplink = document.querySelector(".catalog-popup-open");
+var mapPopup = document.querySelector(".map-popup")
+var mapPopupOpen = document.querySelector(".contacts a")
+var mapCloselink = document.querySelector(".close-window-map")
 catalogPopuplink.addEventListener("mouseover", function(evt) {
   evt.preventDefault()
   catalogPopup.classList.add("catalog-popup")
@@ -14,11 +17,11 @@ catalogPopuplink.addEventListener("mouseout", function(evt) {
 loginLink.addEventListener("click", function (evt) {
     evt.preventDefault();
     writeusPopup.classList.add("modal-show");
-})
+});
 popupclose.addEventListener("click", function(evt){
     evt.preventDefault()
     writeusPopup.classList.remove("modal-show")
-})
+});
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       if (writeusPopup.classList.contains("modal-show")) {
@@ -27,3 +30,13 @@ window.addEventListener("keydown", function (evt) {
       }
     }
   });
+mapPopupOpen.addEventListener("click", function(evt) {
+  evt.preventDefault()
+  mapPopup.classList.remove("close-map")
+  mapPopup.classList.add("modal-show")
+})
+mapCloselink.addEventListener("click", function(evt){
+  evt.preventDefault()
+  mapPopup.classList.remove("modal-show")
+  mapPopup.classList.add("close-map")
+});
